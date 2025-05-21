02372bdfc866925f10be4a7ea09a8536198b1a8e
PxMach <moundjidmachghour58@gmail.com>
2025-05-21 14:47:46 +0100

    Ajout de la fonctionnalité d'affichage dynamique pour les créations dans index.html, permettant de masquer ou d'afficher des éléments supplémentaires. Mise à jour des styles CSS pour gérer la visibilité des éléments et amélioration de la navigation mobile avec des classes actives pour les icônes.

---
+++ src/script.js
@@ -0,0 +1,37 @@
+const show = document.querySelector(".show");
+const gridPos = document.querySelector(".grid_pos");
+const hides = document.querySelectorAll(".hide");
+const navBox = document.querySelector(".nav-box");
+const iconHamburger = document.querySelector(".icon-hamburger");
+const iconClose = document.querySelector(".icon-close");
+const ul = document.querySelector(".nav-header ul");
+const h1 = document.querySelector("h1");
+
+show.addEventListener("click", () => {
+   hides.forEach((hide) => {
+      if (hide.style.display === "none") {
+         hide.style.display = "block";
+         show.textContent = "See less";
+      } else {
+         hide.style.display = "none";
+         show.textContent = "See all";
+      }
+   });
+});
+
+// Menu mobile
+iconHamburger.addEventListener("click", () => {
+   navBox.classList.add("active");
+   iconHamburger.classList.add("disable");
+   iconClose.classList.add("active");
+   ul.classList.add("active");
+   h1.classList.add("disable");
+});
+
+iconClose.addEventListener("click", () => {
+   navBox.classList.remove("active");
+   iconHamburger.classList.remove("disable");
+   iconClose.classList.remove("active");
+   ul.classList.remove("active");
+   h1.classList.remove("disable");
+});
